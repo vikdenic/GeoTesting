0fbfafc6a82ba07c3365002349313dcd604f8546
vikdenic <vik.denic@gmail.com>
2014-11-07 13:40:34 -0600

    Set up UI and obtrain current location

--- GeoTesting/ViewController.swift
+++ GeoTesting/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    override func viewDidLoad() {
+    var coordStringArray = [String]()
+    let locationManager = CLLocationManager()
+
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = NSString(format: "%.0f", slider.value)
+        locationManagerSetUp()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManagerSetUp()
+    {
+        //Requires NSLocationAlwaysUsageDescription key and value in Info.plist
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
     }
 
+    func centerTheMap(location : CLLocation)
+    {
+        let center = location.coordinate
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegionMake(center, span)
+        mapView.setRegion(region, animated: true)
+    }
 
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!)
+    {
+        let newLat = NSString(format: "%.4f", newLocation.coordinate.latitude)
+        let newLong = NSString(format: "%.4f", newLocation.coordinate.longitude)
+
+        let newCoordString = "\(newLat), \(newLong)" as String
+        println(newCoordString)
+        coordStringArray.append(newCoordString)
+
+        centerTheMap(newLocation)
+
+        locationManager.stopUpdatingLocation()
+        locationManager.stopMonitoringSignificantLocationChanges()
+    }
+
+    @IBAction func onUpdateLocationTapped(sender: UIBarButtonItem)
+    {
+        if segmentedControl.selectedSegmentIndex == 0
+        {
+            locationManager.startUpdatingLocation()
+        }
+        else
+        {
+            locationManager.startMonitoringSignificantLocationChanges()
+        }
+    }
+
+    @IBAction func onSliderMoved(sender: UISlider)
+    {
+        title = NSString(format: "%.0f", slider.value)
+    }
 }
 
